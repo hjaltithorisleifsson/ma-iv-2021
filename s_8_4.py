9b65bf7319c8812e1b0e4c9d99ff95b688141414
Hjalti Thor Isleifsson <hisleifsson@student.ethz.ch>
2021-03-17 15:25:43 +0000

    Add s_8_4.py

---
+++ s_8_4.py
@@ -0,0 +1,33 @@
+from fin_diff_ode import *
+import numpy as np 
+import matplotlib.pyplot as plt
+
+a2 = lambda x: -x
+a1 = lambda x: 0.0 * x - 1.0
+a0 = lambda x: 1.0 / x
+f = lambda x: 0.0 * x - 2.0
+
+ode = SecondDegreeODE(a0, a1, a2, f)
+bvo = SecondDegreeBVO(0.0, 1.0, 0.0, 1.0, -2.0, -2.0)
+bvp = SecondDegreeBVP(ode, bvo, 1.0, 2.0)
+
+#errors = []
+#Ns = []
+#for i in range(6):
+#	N = 3 * 10 ** i
+#	Ns.append(N)
+#	x = np.linspace(1,2,N + 1)
+#	c = bvp.fin_diff(N)
+#	u = x * np.log(x) - x
+#	max_error = np.max(np.fabs(u - c))
+#	errors.append(max_error)
+
+#plt.plot(np.log(Ns), np.log(errors))
+#plt.show()
+
+N = 3
+x = np.linspace(1,2,N + 1)
+c = bvp.fin_diff(N)
+u = x * np.log(x) - x
+max_error = np.max(np.fabs(u - c))
+print(max_error)
